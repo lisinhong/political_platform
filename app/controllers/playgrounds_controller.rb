ef628951a67512ebc592713b8d93e7915f366760
JohnLiu <joinmytripweb@gmail.com>
2018-11-19 21:05:45 +0800

    fix result bug

--- app/controllers/playgrounds_controller.rb
+++ app/controllers/playgrounds_controller.rb
@@ -2,12 +2,12 @@ class PlaygroundsController < ApplicationController
 
 
     def index
-        if current_user == nil 
-            redirect_to "/users/auth/facebook"
-            return
-        end
+        # if current_user == nil 
+        #     redirect_to "/users/auth/facebook"
+        #     return
+        # end
         @user = User.new()
-        @share_url = 'http://www.taiwanbunbun.com/result/' + current_user.try(:uid)
+        # @share_url = 'http://www.taiwanbunbun.com/result/' + current_user.try(:uid)
 
         render :template => "politicians/playground"
     end
@@ -24,9 +24,7 @@ class PlaygroundsController < ApplicationController
     end
 
     def result
-
         @user_image_url = User.find_by(uid: params[:uid]).image.url       
-        
         render :template => "politicians/result"
     end
 
@@ -71,12 +69,12 @@ class PlaygroundsController < ApplicationController
         result = Result.new(name: current_user.try(:name), 
                     email: current_user.try(:email),   
                     u_id: current_user.try(:uid),
-                    affairs: type_ids.count(0),
-                    transportation: type_ids.count(1),
-                    economic: type_ids.count(2),
-                    education: type_ids.count(3),
-                    teen: type_ids.count(4),
-                    citizen: type_ids.count(5))
+                    affairs: type_ids.count("0"),
+                    transportation: type_ids.count("1"),
+                    economic: type_ids.count("2"),
+                    education: type_ids.count("3"),
+                    teen: type_ids.count("4"),
+                    citizen: type_ids.count("5"))
         result.save
 
         
